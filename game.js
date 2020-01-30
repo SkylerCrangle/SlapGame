0fa05421f2f3d980ca5a96b14b4641ecef5fbabe
SkylerCrangle <smcrangle@gmail.com>
2020-01-30 14:07:20 -0700

    made goofy variables an object

--- game.js
+++ game.js
@@ -1,37 +1,45 @@
-let health = 100;
-let hits = 0;
-let theName = "Goofy";
+let goofy = {
+  name: "Goofy",
+  health: 100,
+  hits: 0
+}
+
 
 function slap() {
-  health--;
-  if (health > 0) {
-    hits++
+
+  if (goofy.health > 0) {
+    goofy.hits++
   }
+  goofy.health--;
+
   //alert(health);
   update()
 }
 
 function punch() {
-  health -= 5;
-  if (health > 0) {
-    hits++
+
+  if (goofy.health > 0) {
+    goofy.hits++
   }
+  goofy.health -= 5;
   update()
 }
 
 function kick() {
-  health -= 10;
-  if (health > 0) {
-    hits++
+
+  if (goofy.health > 0) {
+    goofy.hits++
   }
+  goofy.health -= 10;
+
   update()
 }
 
 
 function update() {
-  if (health < 0) {
-    health = 0;
+  if (goofy.health < 0) {
+    goofy.health = 0;
   }
-  document.getElementById("health").innerText = (`${theName} is feeling ${health}% because he's been hit ${hits} times!`)
+  document.getElementById("health").innerText = (`${goofy.name} is feeling ${goofy.health}% because he's been hit ${goofy.hits} times!`)
 }
 update()
