116c765003ff18797a6d67124a9035fb7d634029
SkylerCrangle <smcrangle@gmail.com>
2020-01-30 13:57:12 -0700

    finshed readme1

--- game.js
+++ game.js
@@ -1,12 +1,37 @@
 let health = 100;
+let hits = 0;
+let theName = "Goofy";
 
 function slap() {
   health--;
+  if (health > 0) {
+    hits++
+  }
   //alert(health);
   update()
 }
 
+function punch() {
+  health -= 5;
+  if (health > 0) {
+    hits++
+  }
+  update()
+}
+
+function kick() {
+  health -= 10;
+  if (health > 0) {
+    hits++
+  }
+  update()
+}
+
+
 function update() {
-  document.getElementById("health").innerText = (`Goofy is feeling ${health}%`)
+  if (health < 0) {
+    health = 0;
+  }
+  document.getElementById("health").innerText = (`${theName} is feeling ${health}% because he's been hit ${hits} times!`)
 }
 update()
