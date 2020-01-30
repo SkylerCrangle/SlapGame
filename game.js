0b7f744eaf510d9c40000cab6b25629222b60da1
SkylerCrangle <smcrangle@gmail.com>
2020-01-30 13:43:18 -0700

    through step 5

--- game.js
+++ game.js
@@ -2,6 +2,11 @@ let health = 100;
 
 function slap() {
   health--;
-  alert(health);
+  //alert(health);
+  update()
 }
 
+function update() {
+  document.getElementById("health").innerText = (`Goofy is feeling ${health}%`)
+}
+update()
